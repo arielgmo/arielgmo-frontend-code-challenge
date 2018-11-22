8154769194d4821dc15b7ce8dfea39fc3cf53afe
Ariel G M de Oliveira <ariel.oliveira@vistajet.com>
2018-11-22 14:35:50 -0200

    Initial webpack commit - webpack/eslint/redux/structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RootContainer from './Components/RootContainer';
+import './index.css';
+
+ReactDOM.render(<RootContainer />, document.getElementById('root'));
+
+module.hot.accept();
